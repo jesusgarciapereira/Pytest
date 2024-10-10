b9d05a2bbdfb287a6c9931addd07c7147b05cf4c
jgarcia <jgarcia@107-04>
2024-10-10 10:12:18 +0200

    Agregar archivos de proyecto.

---
+++ Problema1/test.py
@@ -0,0 +1,6 @@
+
+from cifradoCesar import cifradoCesar
+
+
+def test_cifradoCesar1():
+    assert cifradoCesar("abc",3) == "def"
---
+++ Pytest/test_suma.py
@@ -0,0 +1,5 @@
+from suma import suma
+
+
+def test_suma():
+    assert suma(2,3) == 5
