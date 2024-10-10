6ba014c65bd1687dd02de9d7bd68c615f77851dc
jgarcia <jgarcia@107-04>
2024-10-10 10:16:37 +0200

    entregado

---
+++ Problema1/text_cifradoCesar.py
@@ -0,0 +1,5 @@
+
+def cifradoCesar(mensaje,clave):
+    alfabeto = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    mensajeCifrado = ""
+    return mensajeCifrado
